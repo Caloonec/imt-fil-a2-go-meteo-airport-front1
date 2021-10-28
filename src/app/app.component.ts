3a409b3fce1026c1a877083dcf459087d06ff05c
Caloonec <cantin.caloone@gmail.com>
2021-10-28 15:17:00 +0200

    1 st real commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ApiService} from "./api.service";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private api: ApiService) {
+    this.ngOnInit();
+  }
+
   title = 'GoAirportFront';
+  testData: Object = [];
+
+  ngOnInit() {
+
+  }
+  getData(){
+    this.api.getTestQuery().subscribe((data) => {
+      this.testData = data;
+    }, (error) => {
+      console.log("An error accessing apiService");
+      console.log(error)
+    })
+
+    console.log(this.testData)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatChipsModule} from "@angular/material/chips";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatChipsModule,
+        MatButtonModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
