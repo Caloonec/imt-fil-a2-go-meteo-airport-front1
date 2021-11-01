130b7dfb54f263ee8eee47c2ec0380ccb52a13f6
Yan IMENSAR <sub@yan-imensar.fr>
2021-11-01 12:58:34 +0100

    :construction: WIP LineChart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatChipsModule} from "@angular/material/chips";
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LineChartComponent,
   ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         MatChipsModule,
         MatButtonModule,
-        HttpClientModule
+        HttpClientModule,
+        ChartsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/line-chart/line-chart.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.css']
+})
+
+
+export class LineChartComponent implements OnInit {
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [65, 100, 80, 81, 56, 55, 40], label: 'Series B' }
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    annotation: null
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType ='line';
+  public lineChartPlugins = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
