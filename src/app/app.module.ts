fc66f250fd12228aeeaa2c8281b1c06b91f547b2
Caloonec <cantin.caloone@gmail.com>
2021-11-03 08:58:45 +0100

    :bug: TOFIX erreurs console sur barCharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,13 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from "@angular/common/http";
 import { ChartsModule } from 'ng2-charts';
 import { LineChartComponent } from './line-chart/line-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LineChartComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
